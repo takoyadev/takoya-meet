39974b50bd3c753bded72df420f2c017db1ad9e6
ymad <ymad@hotmail.fr>
2022-09-11 13:58:59 +0200

    MAke an auth code

--- app/src/main/java/com/example/takoyadev/MainActivity2.kt
+++ app/src/main/java/com/example/takoyadev/MainActivity2.kt
@@ -6,16 +6,23 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 
+
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         val Button: Button = findViewById(R.id.id_button)
-       val email = findViewById(R.id.id_login)
-        val password = findViewById(R.id.id_pass)
+       val email = findViewById<EditText>(R.id.id_login)
+        val password = findViewById<EditText>(R.id.id_pass)
 
-        val data1 = email.text.toString
-        val data2 = password.text.toString()
+        Button.setOnClickListener {
+            val data1 = email.text.toString()
+            val data2 = password.text.toString()
+            if (data1 != null && data2 != null) {
+                println("ok")
+            }
+        }
+
 
 
 
